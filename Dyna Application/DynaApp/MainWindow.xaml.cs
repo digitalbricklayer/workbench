c29305f6470d12f120a9c629614481dff46af5ad
Jack Hughes <jhughes@openxtra.co.uk>
2015-05-27 13:56:07 +0100

    Adds the constraint expression parser.

--- Dyna Application/DynaApp/MainWindow.xaml.cs
+++ Dyna Application/DynaApp/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ namespace DynaApp
         public MainWindow()
         {
             InitializeComponent();
+            menuFileResolve.Click += new RoutedEventHandler(MenuFileResolve_Click);
         }
 
         /// <summary>
@@ -19,6 +20,15 @@ namespace DynaApp
         void FileCloseCommand(object sender, ExecutedRoutedEventArgs args)
         {
             this.Close();
+        }
+
+        /// <summary>
+        /// Resolve the problem contained within the workspace.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuFileResolve_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
