77d12880304a4d678186d9a10ac8ab07d2c1b769
Jack Hughes <jhughes@openxtra.co.uk>
2015-06-16 11:30:13 +0100

    Adds tests for the ConstraintViewModel.IsConnectableTo and DomainViewModel.IsConnectableTo methods.

---
+++ Dyna Application/Dyna.UI.Tests.Unit/ViewModels/ConstraintViewModelTests.cs
@@ -0,0 +1,48 @@
+﻿using DynaApp.ViewModels;
+using NUnit.Framework;
+
+namespace Dyna.UI.Tests.Unit.ViewModels
+{
+    [TestFixture]
+    public class ConstraintViewModelTests
+    {
+        [Test]
+        public void Constraint_Cannot_Connect_To_Another_Constraint()
+        {
+            // Arrange
+            var sut = new ConstraintViewModel("X");
+
+            // Act
+            var actualResult = sut.IsConnectableTo(new ConstraintViewModel("Y"));
+
+            // Assert
+            Assert.That(actualResult, Is.False);
+        }
+
+        [Test]
+        public void Constraint_Cannot_Connect_To_A_Variable()
+        {
+            // Arrange
+            var sut = new ConstraintViewModel("X");
+
+            // Act
+            var actualResult = sut.IsConnectableTo(new VariableViewModel("Y"));
+
+            // Assert
+            Assert.That(actualResult, Is.False);
+        }
+
+        [Test]
+        public void Constraint_Cannot_Connect_To_A_Domain()
+        {
+            // Arrange
+            var sut = new ConstraintViewModel("X");
+
+            // Act
+            var actualResult = sut.IsConnectableTo(new DomainViewModel("Y"));
+
+            // Assert
+            Assert.That(actualResult, Is.False);
+        }
+    }
+}
---
+++ Dyna Application/Dyna.UI.Tests.Unit/ViewModels/DomainViewModelTests.cs
@@ -0,0 +1,48 @@
+﻿using DynaApp.ViewModels;
+using NUnit.Framework;
+
+namespace Dyna.UI.Tests.Unit.ViewModels
+{
+    [TestFixture]
+    public class DomainViewModelTests
+    {
+        [Test]
+        public void Domain_Cannot_Connect_To_Another_Domain()
+        {
+            // Arrange
+            var sut = new DomainViewModel("X");
+
+            // Act
+            var actualResult = sut.IsConnectableTo(new DomainViewModel("Y"));
+
+            // Assert
+            Assert.That(actualResult, Is.False);
+        }
+
+        [Test]
+        public void Domain_Cannot_Connect_To_A_Variable()
+        {
+            // Arrange
+            var sut = new DomainViewModel("X");
+
+            // Act
+            var actualResult = sut.IsConnectableTo(new VariableViewModel("Y"));
+
+            // Assert
+            Assert.That(actualResult, Is.False);
+        }
+
+        [Test]
+        public void Domain_Cannot_Connect_To_A_Constraint()
+        {
+            // Arrange
+            var sut = new DomainViewModel("X");
+
+            // Act
+            var actualResult = sut.IsConnectableTo(new ConstraintViewModel("Y"));
+
+            // Assert
+            Assert.That(actualResult, Is.False);
+        }
+    }
+}
