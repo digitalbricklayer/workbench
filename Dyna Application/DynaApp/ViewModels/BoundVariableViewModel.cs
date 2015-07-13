82890bdf0c9066ee1ae04e2dc94b9769d11e8e04
Jack Hughes <jhughes@openxtra.co.uk>
2015-07-13 17:58:18 +0100

    Implements the simplest solution view I could think of.

--- Dyna Application/DynaApp/ViewModels/BoundVariableViewModel.cs
+++ Dyna Application/DynaApp/ViewModels/BoundVariableViewModel.cs
@@ -1,6 +1,67 @@
-﻿namespace DynaApp.ViewModels
+﻿using System;
+
+namespace DynaApp.ViewModels
 {
+    /// <summary>
+    /// View model for a bound variable.
+    /// </summary>
     public sealed class BoundVariableViewModel : AbstractViewModel
     {
+        private int value;
+        private VariableViewModel variable;
+        private string name;
+
+        /// <summary>
+        /// Initialize the bound variable with a variable.
+        /// </summary>
+        /// <param name="theVariable">Variable to bind the value to.</param>
+        public BoundVariableViewModel(VariableViewModel theVariable)
+        {
+            if (theVariable == null)
+                throw new ArgumentNullException("theVariable");
+            this.Variable = theVariable;
+        }
+
+        /// <summary>
+        /// Gets or sets the variable name.
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the variable.
+        /// </summary>
+        public VariableViewModel Variable
+        {
+            get { return this.variable; }
+            set
+            {
+                this.variable = value;
+                OnPropertyChanged("Variable");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the bound value.
+        /// </summary>
+        public int Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                this.value = value;
+                OnPropertyChanged("Value");
+            }
+        }
     }
 }
--- Dyna Application/DynaApp/ViewModels/SolutionViewModel.cs
+++ Dyna Application/DynaApp/ViewModels/SolutionViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace DynaApp.ViewModels
 {
@@ -7,17 +9,42 @@ namespace DynaApp.ViewModels
     /// </summary>
     public sealed class SolutionViewModel : AbstractViewModel
     {
-        private readonly List<BoundVariableViewModel> values = new List<BoundVariableViewModel>();
+        /// <summary>
+        /// Initialize the solution with bound values.
+        /// </summary>
+        /// <param name="theValues">Bound values.</param>
+        public SolutionViewModel(IEnumerable<BoundVariableViewModel> theValues)
+        {
+            if (theValues == null)
+                throw new ArgumentNullException("theValues");
+            this.Values = new ObservableCollection<BoundVariableViewModel>(theValues);
+        }
+
+        /// <summary>
+        /// Initialize the solution with default values.
+        /// </summary>
+        public SolutionViewModel()
+        {
+            this.Values = new ObservableCollection<BoundVariableViewModel>();
+        }
 
         /// <summary>
         /// Gets the values displayed in the solution.
         /// </summary>
-        public IEnumerator<BoundVariableViewModel> Values
+        public ObservableCollection<BoundVariableViewModel> Values
         {
-            get
-            {
-                return this.values.GetEnumerator();
-            }
+            get; private set;
+        }
+
+        /// <summary>
+        /// Bind the bound values to the solution.
+        /// </summary>
+        /// <param name="theValues">Bound values.</param>
+        public void BindTo(IEnumerable<BoundVariableViewModel> theValues)
+        {
+            this.Values.Clear();
+            foreach (var value in theValues)
+                this.Values.Add(value);
         }
     }
 }
