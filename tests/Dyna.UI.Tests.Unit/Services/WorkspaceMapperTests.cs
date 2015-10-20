c8db50d846e1d58ad54e540263abf18c1b5db12f
digitalbricklayer <jhughes@openxtra.co.uk>
2015-10-20 17:17:55 +0100

    Renames the ModelServiceTests fixture to match the SUT.

---
+++ tests/Dyna.UI.Tests.Unit/Services/WorkspaceMapperTests.cs
@@ -0,0 +1,25 @@
+﻿using DynaApp.Services;
+using NUnit.Framework;
+
+namespace Dyna.UI.Tests.Unit.Services
+{
+    [TestFixture]
+    public class WorkspaceMapperTests
+    {
+        [Test]
+        public void MapFrom_With_Valid_Model_Returns_Expected_View_Model()
+        {
+            var sut = new WorkspaceMapper(new ModelViewModelCache());
+            var actualWorkspaceModel = sut.MapFrom(WorkspaceModelFactory.Create());
+            Assert.That(actualWorkspaceModel, Is.Not.Null);
+        }
+
+        [Test]
+        public void MapFrom_With_Valid_Model_Sets_Expected_Workspace_Model()
+        {
+            var sut = new WorkspaceMapper(new ModelViewModelCache());
+            var actualWorkspaceModel = sut.MapFrom(WorkspaceModelFactory.Create());
+            Assert.That(actualWorkspaceModel.WorkspaceModel, Is.Not.Null);
+        }
+    }
+}
