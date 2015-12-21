0a3daa51d456e77d4561fc85fa7c8f7432a75287
digitalbricklayer <jhughes@openxtra.co.uk>
2015-12-21 16:56:08 +0000

    Ports the code behind in the main window view to use view model commands.

--- src/Workbench/Views/MainWindowView.xaml.cs
+++ src/Workbench/Views/MainWindowView.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Input;
-using Workbench.ViewModels;
 
 namespace Workbench.Views
 {
@@ -12,43 +10,6 @@ namespace Workbench.Views
         public MainWindowView()
         {
             InitializeComponent();
-        }
-
-        /// <summary>
-        /// Gets the main window view model.
-        /// </summary>
-        public MainWindowViewModel ViewModel
-        {
-            get
-            {
-                return (MainWindowViewModel)this.DataContext;
-            }
-        }
-
-        /// <summary>
-        /// Event raised to delete the selected graphics.
-        /// </summary>
-        private void DeleteSelectedGraphics_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            this.ViewModel.Workspace.DeleteSelectedGraphics();
-        }
-
-        private void DeleteVariable_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            var variable = (VariableViewModel)e.Parameter;
-            this.ViewModel.Workspace.Model.DeleteVariable(variable);
-        }
-
-        private void DeleteDomain_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            var domain = (DomainViewModel)e.Parameter;
-            this.ViewModel.Workspace.Model.DeleteDomain(domain);
-        }
-
-        private void DeleteConstraint_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            var constraint = (ConstraintViewModel)e.Parameter;
-            this.ViewModel.Workspace.Model.DeleteConstraint(constraint);
         }
     }
 }
