6546279c74db348e5819ab6960c132a53578f9af
Jack Hughes <jhughes@openxtra.co.uk>
2016-02-23 17:38:16 +0000

    Adds code contract to the IAppRuntime interface.

--- src/Workbench/IAppRuntime.cs
+++ src/Workbench/IAppRuntime.cs
@@ -1,7 +1,10 @@
-﻿using Workbench.ViewModels;
+﻿using System;
+using System.Diagnostics.Contracts;
+using Workbench.ViewModels;
 
 namespace Workbench
 {
+    [ContractClass(typeof(IAppRuntimeContract))]
     public interface IAppRuntime
     {
         /// <summary>
@@ -23,5 +26,68 @@ namespace Workbench
         /// Gets the program name.
         /// </summary>
         string ApplicationName { get; }
+    }
+
+    /// <summary>
+    /// Code contract for the IDataService interface.
+    /// </summary>
+    [ContractClassFor(typeof(IAppRuntime))]
+    internal abstract class IAppRuntimeContract : IAppRuntime
+    {
+        private readonly string _applicationName = String.Empty;
+        private string _currentFileName;
+        private ShellViewModel _shell;
+        private WorkspaceViewModel _workspace;
+
+        public WorkspaceViewModel Workspace
+        {
+            get
+            {
+                Contract.Ensures(Contract.Result<WorkspaceViewModel>() != null);
+                return _workspace;
+            }
+            set
+            {
+                Contract.Requires<ArgumentNullException>(value != null);
+                _workspace = value;
+            }
+        }
+
+        public ShellViewModel Shell
+        {
+            get
+            {
+                Contract.Ensures(Contract.Result<ShellViewModel>() != null);
+                return _shell;
+            }
+            set
+            {
+                Contract.Requires<ArgumentNullException>(value != null);
+                _shell = value;
+            }
+        }
+
+        public string CurrentFileName
+        {
+            get
+            {
+                Contract.Ensures(Contract.Result<string>() != null);
+                return _currentFileName;
+            }
+            set
+            {
+                Contract.Requires<ArgumentNullException>(!string.IsNullOrWhiteSpace(value));
+                _currentFileName = value;
+            }
+        }
+
+        public string ApplicationName
+        {
+            get
+            {
+                Contract.Ensures(Contract.Result<string>() != null);
+                return _applicationName;
+            }
+        }
     }
 }
