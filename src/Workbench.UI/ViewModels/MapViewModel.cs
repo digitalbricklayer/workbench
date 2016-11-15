8fbd85c3f00da7caf241a03795ac6da0c6592648
Jack Hughes <jhughes@openxtra.co.uk>
2016-11-15 17:31:27 +0000

    Fixes the default background image in the map visualizer.

--- src/Workbench.UI/ViewModels/MapViewModel.cs
+++ src/Workbench.UI/ViewModels/MapViewModel.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Diagnostics.Contracts;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using Caliburn.Micro;
 using Workbench.Core.Models;
 
@@ -10,21 +7,17 @@ namespace Workbench.ViewModels
 {
     public class MapViewModel : Screen
     {
+        public const string DefaultBackgroundImagePath = "pack://application:,,,/ConstraintWorkbench;component/Images/Transparent.png";
         private readonly MapModel model;
-        private Image backgroundImage;
 
         public MapViewModel(MapModel theModel)
         {
             Contract.Requires<ArgumentNullException>(theModel != null);
             this.model = theModel;
-            this.backgroundImage = new Image();
-            if (this.model.HasBackgroundImage)
+            if (!this.model.HasBackgroundImage)
             {
-                LoadCustomBackgroundImage();
-            }
-            else
-            {
-                LoadDefaultBackgroundImage();
+                // Load a default background
+                this.model.BackgroundImagePath = DefaultBackgroundImagePath;
             }
         }
 
@@ -34,43 +27,6 @@ namespace Workbench.ViewModels
         public MapModel Map
         {
             get { return this.model; }
-        }
-
-        /// <summary>
-        /// Gets or sets the map background image.
-        /// </summary>
-        public Image BackgroundImage
-        {
-            get { return this.backgroundImage; }
-            set
-            {
-                this.backgroundImage = value;
-                NotifyOfPropertyChange();
-            }
-        }
-
-        private void LoadCustomBackgroundImage()
-        {
-            Contract.Ensures(this.model.HasBackgroundImage);
-            var src = new BitmapImage();
-            src.BeginInit();
-            src.UriSource = new Uri(this.model.BackgroundImagePath, UriKind.Absolute);
-            src.CacheOption = BitmapCacheOption.OnLoad;
-            src.EndInit();
-            BackgroundImage.Source = src;
-            BackgroundImage.Stretch = Stretch.Uniform;
-        }
-
-        private void LoadDefaultBackgroundImage()
-        {
-            Contract.Ensures(!this.model.HasBackgroundImage);
-            var src = new BitmapImage();
-            src.BeginInit();
-            src.UriSource = new Uri("../../../Images/Transparent.png", UriKind.Relative);
-            src.CacheOption = BitmapCacheOption.OnLoad;
-            src.EndInit();
-            BackgroundImage.Source = src;
-            BackgroundImage.Stretch = Stretch.Uniform;
         }
     }
 }
