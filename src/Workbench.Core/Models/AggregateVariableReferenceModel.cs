0a57f681e8480cb1320fe3f15eb0f2fc28cf6851
Jack Hughes <jhughes@openxtra.co.uk>
2017-10-30 17:53:46 +0000

    Renames the Aggregate variable reference to use the standard model naming scheme.

---
+++ src/Workbench.Core/Models/AggregateVariableReferenceModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Workbench.Core.Models
+{
+    /// <summary>
+    /// Reference to an aggregate variable.
+    /// </summary>
+    [Serializable]
+    public class AggregateVariableReferenceModel
+    {
+        public AggregateVariableReferenceModel(string newIdentifier, int aggregateIndex)
+        {
+            this.IdentifierName = newIdentifier;
+            this.Index = aggregateIndex;
+        }
+
+        public AggregateVariableReferenceModel()
+        {
+            this.IdentifierName = String.Empty;
+        }
+
+        public int Index { get; private set; }
+
+        public string IdentifierName { get; private set; }
+    }
+}
