381173353ad83629e11a406b633a24e64b842feb
Jack Hughes <jhughes@openxtra.co.uk>
2018-07-27 16:20:58 +0100

    Removes the now redundant Model > Resize Aggregate menu item.

--- src/Workbench.UI/ViewModels/ModelMenuViewModel.cs
+++ src/Workbench.UI/ViewModels/ModelMenuViewModel.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Diagnostics.Contracts;
-using System.Linq;
 using System.Windows.Input;
-using Caliburn.Micro;
 
 namespace Workbench.ViewModels
 {
@@ -11,19 +9,14 @@ namespace Workbench.ViewModels
     /// </summary>
     public class ModelMenuViewModel
     {
-        private readonly IWindowManager windowManager;
         private readonly IAppRuntime appRuntime;
         private readonly TitleBarViewModel titleBar;
 
-        public ModelMenuViewModel(IWindowManager theWindowManager,
-                                  IAppRuntime theAppRuntime,
-                                  TitleBarViewModel theTitleBarViewModel)
+        public ModelMenuViewModel(IAppRuntime theAppRuntime, TitleBarViewModel theTitleBarViewModel)
         {
-            Contract.Requires<ArgumentNullException>(theWindowManager != null);
             Contract.Requires<ArgumentNullException>(theAppRuntime != null);
             Contract.Requires<ArgumentNullException>(theTitleBarViewModel != null);
 
-            this.windowManager = theWindowManager;
             this.appRuntime = theAppRuntime;
             this.titleBar = theTitleBarViewModel;
 
@@ -38,17 +31,14 @@ namespace Workbench.ViewModels
         /// <summary>
         /// Gets the work area view model.
         /// </summary>
-        public WorkspaceViewModel Workspace
-        {
-            get { return this.appRuntime.Workspace; }
-        }
+        public WorkspaceViewModel Workspace => this.appRuntime.Workspace;
 
         /// <summary>
         /// Solve the model.
         /// </summary>
         private void ModelSolveAction()
         {
-            this.Workspace.SolveModel();
+            Workspace.SolveModel();
             this.titleBar.UpdateTitle();
         }
     }
