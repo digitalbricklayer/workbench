17375605795ce5cc2824decd5829f24b133e67a3
Jack Hughes <jhughes@openxtra.co.uk>
2018-08-30 17:08:30 +0100

    Adds the ExpressionConstraintModelItemViewModel test.

---
+++ tests/Workbench.UI.Tests.Unit/ViewModels/ExpressionConstraintModelItemViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using Moq;
+using NUnit.Framework;
+using Workbench.Core.Models;
+using Workbench.ViewModels;
+
+namespace Workbench.UI.Tests.Unit.ViewModels
+{
+    [TestFixture]
+    public class ExpressionConstraintModelItemViewModelTests
+    {
+        [Test]
+        public void Initialize_With_Default_Values_Text_Is_Empty()
+        {
+            var sut = new ExpressionConstraintModelItemViewModel(new ExpressionConstraintModel(), Mock.Of<IWindowManager>());
+            Assert.That(sut.ExpressionText, Is.Empty);
+        }
+    }
+}
