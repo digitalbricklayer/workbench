36c9583e932da8118fcc456f805885452377c2a1
Jack Hughes <jhughes@openxtra.co.uk>
2018-10-25 18:00:52 +0100

    Cleans up the domain model.

--- src/Workbench.Core/Models/DomainModel.cs
+++ src/Workbench.Core/Models/DomainModel.cs
@@ -6,45 +6,17 @@ namespace Workbench.Core.Models
     [Serializable]
     public class DomainModel : Model
     {
-        private DomainExpressionModel expression;
+        private DomainExpressionModel _expression;
 
         /// <summary>
-        /// Initialize a domain with a domain expression.
+        /// Initialize a domain with a name and domain expression.
         /// </summary>
         public DomainModel(ModelName theName, DomainExpressionModel theExpression)
             : base(theName)
         {
-            Contract.Requires<ArgumentNullException>(theExpression != null);
-            this.expression = theExpression;
-        }
-
-        /// <summary>
-        /// Initialize a domain with a domain expression.
-        /// </summary>
-        public DomainModel(ModelName theName)
-            : base(theName)
-        {
             Contract.Requires<ArgumentNullException>(theName != null);
-            this.expression = new DomainExpressionModel();
-        }
-
-        /// <summary>
-        /// Initialize a domain with a domain expression.
-        /// </summary>
-        public DomainModel(DomainExpressionModel theExpression)
-            : base(new ModelName())
-        {
             Contract.Requires<ArgumentNullException>(theExpression != null);
-            this.expression = theExpression;
-        }
-
-        /// <summary>
-        /// Initialize a domain with default values.
-        /// </summary>
-        public DomainModel()
-            : base(new ModelName())
-        {
-            Expression = new DomainExpressionModel();
+            _expression = theExpression;
         }
 
         /// <summary>
@@ -52,22 +24,12 @@ namespace Workbench.Core.Models
         /// </summary>
         public DomainExpressionModel Expression
         {
-            get { return this.expression; }
+            get => _expression;
             set
             {
-                this.expression = value;
+                _expression = value;
                 OnPropertyChanged();
             }
-        }
-
-        /// <summary>
-        /// Parse a raw expression.
-        /// </summary>
-        /// <param name="rawExpression">Raw domain expression.</param>
-        public void ParseExpression(string rawExpression)
-        {
-            Contract.Requires<ArgumentException>(!string.IsNullOrWhiteSpace(rawExpression));
-            Expression = new DomainExpressionModel(rawExpression);
         }
     }
 }
