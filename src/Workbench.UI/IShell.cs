84c526b59ef9bc00dead9165a15f5ffcf4876a71
Jack Hughes <jhughes@openxtra.co.uk>
2018-11-26 17:08:51 +0000

    Moves the shell interface into the Workbench UI root namespace.

---
+++ src/Workbench.UI/IShell.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using Workbench.ViewModels;
+
+namespace Workbench
+{
+    [ContractClass(typeof(IShellContract))]
+    public interface IShell
+    {
+        /// <summary>
+        /// Gets or sets the work area.
+        /// </summary>
+        WorkspaceViewModel Workspace { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current workspace document.
+        /// </summary>
+        WorkspaceDocumentViewModel CurrentDocument { get; set; }
+
+        /// <summary>
+        /// Close the shell with the option to cancel.
+        /// </summary>
+        void Close(CancelEventArgs cancelEventArgs);
+
+        /// <summary>
+        /// Close the shell.
+        /// </summary>
+        void Close();
+    }
+}
